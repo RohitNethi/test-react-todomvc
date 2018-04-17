c03391dcf25cf949d031a23e515e8d886a2c140a
RohitNethi <snethi@uncc.edu>
2018-04-17 18:55:14 -0400

    setup folders and config

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var path = require('path');
+var webpack = require('webpack');
+
+
+module.exports = {
+    entry: __dirname + '/src/app.js',
+    output: {
+        path: path.resolve(__dirname, 'assets'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.scss$/,
+                use: [{
+                    loader: "style-loader" // creates style nodes from JS strings
+                }, {
+                    loader: "css-loader" // translates CSS into CommonJS
+                }, {
+                    loader: "sass-loader",// compiles Sass to CSS
+                }]
+            }]
+    },
+    stats: {
+        colors: true
+    },
+    devtool: 'source-map'
+};
